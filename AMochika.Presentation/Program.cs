e544c489226aac128121d108286ba06d4deb9a59
Mochika <yevgen.letin@gmail.com>
2024-12-23 16:57:33 +0100

    ADD Swagger

--- AMochika.Presentation/Program.cs
+++ AMochika.Presentation/Program.cs
@@ -43,15 +43,14 @@ builder.Services.AddAutoMapper(typeof(MappingProfile));
 var app = builder.Build();
 
 
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
         c.RoutePrefix = string.Empty; // Hace que Swagger sea la página principal
     });
-}
+
 
 app.MapControllers();
 
